f5164294443f832fd6beedbe10fa039988414c39
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-08-26 16:41:15 +0200

    Action Results

--- MovieRentalAppMVC/Controllers/MoviesController.cs
+++ MovieRentalAppMVC/Controllers/MoviesController.cs
@@ -19,5 +19,32 @@ namespace MovieRentalAppMVC.Controllers
 
             return View(movie);
         }
+
+
+
+
+
+        /*
+                ActionResults which are the outputs of actions
+                Below some helper methods of ActionResult
+
+
+                // GET: Movies/ActionResultHelperMethods
+                public ActionResult ActionResultHelperMethods()
+                {
+                    var movie = new Movie() { Name = "Shrek" };
+
+
+                    //return View(movie);
+                    //return Content("bla bla bla content view helper method");
+                    //return Redirect("http://google.com");
+                    //return RedirectToAction("Index", "Home");
+                    //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "someFilter" });
+                    //return HttpNotFound();
+                    //return new EmptyResult();
+                    //return Json(...);
+                    //return File(...)
+                }
+        */
     }
 }
