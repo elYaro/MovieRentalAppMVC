cfa13c5c73b9444751ac4fa21f67d8ca908f8f8a
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-08-26 16:11:25 +0200

    Getting Started: Random movie action and view added

---
+++ MovieRentalAppMVC/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using MovieRentalAppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalAppMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            return View(movie);
+        }
+    }
+}
