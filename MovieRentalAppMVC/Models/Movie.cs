ce031fc3e8ab047636f564c9d734fba3b1cb5f3c
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-02 14:44:45 +0200

    Add data annotations to some movie properties

--- MovieRentalAppMVC/Models/Movie.cs
+++ MovieRentalAppMVC/Models/Movie.cs
@@ -14,15 +14,19 @@ namespace MovieRentalAppMVC.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Release")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Date of Adding")]
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
 
         [Required]
         public Genre Genre { get; set; }
 
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
     }
