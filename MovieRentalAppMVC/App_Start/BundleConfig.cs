636368b65929992d7dc976451df7f93edfbb67c2
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-10 16:10:23 +0200

    using bootbox plug-in for confirmation box in delete customer

--- MovieRentalAppMVC/App_Start/BundleConfig.cs
+++ MovieRentalAppMVC/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace MovieRentalAppMVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       //"~/Content/bootstrap.css",
