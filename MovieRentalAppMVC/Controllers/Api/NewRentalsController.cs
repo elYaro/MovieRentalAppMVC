ebe004d7403563b20c3526a7daae469a2de069f3
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-18 19:50:24 +0200

    NewRental Feature: add NewRentalsController and NewRentalDto

---
+++ MovieRentalAppMVC/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,22 @@
+﻿using MovieRentalAppMVC.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieRentalAppMVC.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        // /api/newrentalscontroller/createnewrental
+        [HttpPost]
+        public IHttpActionResult CreateNewRental (NewRentalDto newRental)
+        {
+            return NotFound();
+        }
+
+        
+    }
+}
