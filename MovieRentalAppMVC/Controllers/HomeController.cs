69828fcb42a703cb82e3ccf8ae4aa77dc422c84f
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-18 13:33:44 +0200

    Output  Cash , which is for cashing the rendered html

--- MovieRentalAppMVC/Controllers/HomeController.cs
+++ MovieRentalAppMVC/Controllers/HomeController.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MovieRentalAppMVC.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        /*
+         * Allowing Cashing: 
+         * Duration -> how long data in cashe is stored, 
+         * Location -> we can cashe data on the server (if view is NOT specific to the given user) or on the client (if view is specific to the given user),
+         * VaryByParam -> if this action takes one or more parameters, and the output changes based on the value of these parameters, we can cashe each output separately ex. for specific param = "genre" , for all params ="*"
+         
+        [OutputCache(Duration =20, Location = OutputCacheLocation.Server, VaryByParam = "*" )]
+        */
+
+        /*
+         * in order to DISABLE CASHING we change the above line into this:
+         Duration -> to zero (0)
+         NoStore = true
+         
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
+        */
         public ActionResult Index()
         {
             return View();
