743eba9da7410069dc01911470400b018150349e
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-19 22:15:41 +0200

    Add basig New Rental Form  for New Rental action

---
+++ MovieRentalAppMVC/Controllers/RentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalAppMVC.Controllers
+{
+    public class RentalsController : Controller
+    {
+        
+        public ActionResult New()
+        {
+
+
+            return View();
+        }
+    }
+}
