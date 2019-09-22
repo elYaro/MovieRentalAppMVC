6fc0e5c6d7cc9155bb170b4ee6bcc71320dba450
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-09-22 16:04:44 +0200

    Add Filtering (numberInStock >0 and name filtering) the records of movies in New Rental View

--- MovieRentalAppMVC/Controllers/Api/MoviesController.cs
+++ MovieRentalAppMVC/Controllers/Api/MoviesController.cs
@@ -21,21 +21,29 @@ namespace MovieRentalAppMVC.Controllers.Api
         }
 
         //GET: /api/movies
-        public IHttpActionResult GetAllMovies()
+        public IHttpActionResult GetAllMovies(string query = null)
         {
-            var movies = _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
+                .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+            }
+
+            var movieDto = moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
 
-            if (movies == null)
+            if (movieDto == null)
             {
                 //throw new HttpResponseException(HttpStatusCode.NotFound);
                 return NotFound();
             }
 
             //return movies;
-            return Ok(movies);
+            return Ok(movieDto);
         }
 
 
